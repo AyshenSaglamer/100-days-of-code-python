b971d8c41416b8a7d917eb7493a85f006f4d825d
AyshenSaglamer <100617236+AyshenSaglamer@users.noreply.github.com>
2025-07-19 19:37:16 +0300

    Update and rename Day 1 to Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,13 @@
+# Day 1 - July 19, 2025
+# PyCharm Setup
+# Print
+# Input Function
+# Variable Naming
+
+
+# First Project: Band Name Generator
+
+print("Welcome to the Band Name Generator!")
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name is " + city_name + " " + pet_name)
